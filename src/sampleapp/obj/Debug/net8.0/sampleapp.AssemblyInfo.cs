f113e8f5e5901f755bbd89403c160b3bf2b00f06
sxy <1301621323@qq.com>
2024-10-06 20:16:53 +0800

    laptap advance

--- src/sampleapp/obj/Debug/net8.0/sampleapp.AssemblyInfo.cs
+++ src/sampleapp/obj/Debug/net8.0/sampleapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sampleapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7388cd2aa955711ec422b0fe60a2711c8f1167")]
 [assembly: System.Reflection.AssemblyProductAttribute("sampleapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sampleapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
